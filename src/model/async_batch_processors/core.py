a89138361f76bfa11ad1454e474dac40d3f6079d
Chris O'Quinn <chrisoquinn.2@gmail.com>
2024-09-01 12:29:04 -0700

    Making modifications to the async core.

--- src/model/async_batch_processors/core.py
+++ src/model/async_batch_processors/core.py
@@ -13,7 +13,7 @@ from typing import Dict, Tuple, Callable, List, Optional
 from .clustering import constrained_kmeans, assign_best_to_centroid
 from ..data import ActionRequest
 
-RequestBuffer = Dict[str, Tuple[asyncio.Future, ActionRequest]]
+RequestBuffer = Dict[str, Tuple[Callable, ActionRequest]]
 LoggingCallback = Callable[[str, int], None]
 
 SHAPES_NAME = "shape"
@@ -329,7 +329,7 @@ class TransformerBatchStrategy(BatchStrategy):
         super().__init__(clustering_strategy)
         self.use_shape_info = use_shape_info
         self.use_context_info = use_context_info
-    def get_vital_statistics(self, requests: RequestBuffer) ->torch.Tensor:
+    def get_vital_statistics(self, requests: RequestBuffer) ->Dict[str, torch.Tensor]:
         vitals = {}
         for key, (_, request) in requests.items():
             statistics = []
@@ -347,7 +347,7 @@ class TransformerBatchStrategy(BatchStrategy):
                 statistics.append(context.shape[0])
 
             # Combine together. Then store
-            statistics = torch.stack(statistics)
+            statistics = torch.tensor(statistics)
             vitals[key] = statistics
         return vitals
 
@@ -387,6 +387,7 @@ class BatchAssembly(ABC):
             - metadata: A list of List[Tuple[UUID, Callable]], where each inner list
                         corresponds to an entry in the batch and associates the UUIDs
                         with their callbacks.
+        :effect: Modifies the request buffer to remove used requests.
         """
         pass
 
@@ -412,14 +413,99 @@ class TransformerAssembly(BatchAssembly):
     def __init__(self,
                  include_context: bool,
                  include_shapes: bool,
-                 include_targets: bool
+                 include_targets: bool,
+                 num_channel_dim: int
                  ):
         """
         :param include_context: Whether to include context info in the output dictionary
         :param include_shapes: Whether to include shape info in the output dictionary
         :param include_targets: Whether to include target info in the output dictionary.
+        :param num_channel_dim: The number of channel dimensions.
+                                  Everything before that will be flattened.
+
         """
         super().__init__()
+
         self.include_context = include_context
         self.include_shape = include_shapes
         self.include_target = include_targets
+        self.num_embedding_dim = num_channel_dim
+
+    def __call__(self,
+                 request_buffer: RequestBuffer,
+                 uuids: List[str],
+                 logging_callback: LoggingCallback
+                 ) -> Tuple[Dict[str, torch.Tensor], List[Tuple[str, Callable]]]:
+        """
+        Assembles a batch of data from the selected requests.
+
+        :param request_buffer: The Request Buffer containing pending requests.
+        :param uuids: A list of UUIDs selected by the Batching Strategy.
+        :param logging_callback: A callback function for logging, which accepts a
+                                 message and a verbosity level.
+        :return:
+            - batch: A fully formed batch, which could be a tensor (or a set of tensors)
+                     ready for processing. Implementers should handle any necessary padding.
+            - metadata: A list of List[Tuple[UUID, Callable]], where each inner list
+                        corresponds to an entry in the batch and associates the UUIDs
+                        with their callbacks.
+        :effect: Modifies the request buffer to remove used
+        """
+
+        # Define content accumulators
+        metadata = []
+        shapes = [] if self.include_shape else None
+        targets = [] if self.include_target else None
+        contexts = [] if self.include_context else None
+
+        # Define max length trackers. These will be used in the padding
+        # step.
+
+        targets_max_length = 0 if self.include_target else None
+        context_max_length = 0 if self.include_context else None
+
+        # Go and get all the information.
+        #
+        # Track as well the padding targets, to whatever degree is relevant
+        for uuid in uuids:
+            future, action_request = request_buffer.pop(uuid)
+            metadata.append((uuid, future))
+
+            if self.include_context:
+                # Handle context extraction. This includes updating the padding targets
+                # and getting the actual information
+                context = action_request.state_tracker.context
+                context_max_length = max(context_max_length, context.shape[0])
+                contexts.append(context)
+
+            if self.include_shape:
+                # Handle shape extraction. Shapes also are used to track target
+                # lengths
+                shape = action_request.subtask_details[SHAPES_NAME]
+                shapes.append(shape)
+
+            if self.include_target:
+                target = action_request.subtask_details[TARGETS_NAME]
+                target = target.flatten(0, -(self.num_embedding_dim + 1))
+                targets_max_length = max(targets_max_length, target.shape[0])
+
+        batched = {}
+        if self.include_shape:
+            shapes = torch.stack(shapes, dim=0)
+            batched["shape"] = shapes
+
+        if self.include_context:
+            contexts = [F.pad(context, (0, context_max_length - context.shape[0])) for context in contexts]
+            contexts = torch.stack(contexts, dim=0)
+            batched["context"] = contexts
+
+        if self.include_target:
+            targets = [F.pad(target, (0, targets_max_length-target.shape[0])) for target in targets]
+            targets = torch.stack(targets, dim=0)
+            batched["targets"] = targets
+
+        return batched, metadata
+
+
+
+
